81c5fe310c00e18848a752ec34975a35f439c12a
Nicholas Devenish <ndevenish@gmail.com>
2024-08-07 22:28:24 +0100

    Add predefined unit validators for each NX unit
    NX_ANY and NX_TRANSFORMATION are special and so will be handled separately.

--- src/nexus/core.py
+++ src/nexus/core.py
@@ -16,10 +16,44 @@ def _check_quantity_type(is_type: type, value: Quantity) -> Quantity:
     return value
 
 
-class NXUnit:
+class Units:
     """Validators for validating the dimensionality of a given quantity"""
 
+    # "NX_ANY": None,
+    # # NX_TRANSFORMATION is special, because it changes depending on the type of transform
+    # "NX_TRANSFORMATION": None,
+
+    ANGLE = AfterValidator(partial(_check_dimension, "[]"))
+    AREA = AfterValidator(partial(_check_dimension, "[area]"))
+    CROSS_SECTION = AfterValidator(partial(_check_dimension, "[area]"))
+    CHARGE = AfterValidator(partial(_check_dimension, "[charge]"))
+    CURRENT = AfterValidator(partial(_check_dimension, "[current]"))
+    DIMENSIONLESS = AfterValidator(partial(_check_dimension, "[]"))
+    EMITTANCE = AfterValidator(partial(_check_dimension, "[length] * [area]"))
+    ENERGY = AfterValidator(partial(_check_dimension, "[energy]"))
+    FLUX = AfterValidator(partial(_check_dimension, "1 / [time] / [area]"))
+    FREQUENCY = AfterValidator(partial(_check_dimension, "[frequency]"))
     LENGTH = AfterValidator(partial(_check_dimension, "[length]"))
+    MASS = AfterValidator(partial(_check_dimension, "[mass]"))
+    MASS_DENSITY = AfterValidator(partial(_check_dimension, "[density]"))
+    MOLECULAR_WEIGHT = AfterValidator(partial(_check_dimension, "[mass] / [substance]"))
+    PER_AREA = AfterValidator(partial(_check_dimension, "1 / [area]"))
+    PER_LENGTH = AfterValidator(partial(_check_dimension, "1 / [length]"))
+    PERIOD = AfterValidator(partial(_check_dimension, "[time]"))
+    POWER = AfterValidator(partial(_check_dimension, "[power]"))
+    PRESSURE = AfterValidator(partial(_check_dimension, "[pressure]"))
+    PULSES = AfterValidator(partial(_check_dimension, "[]"))
+    COUNT = AfterValidator(partial(_check_dimension, "[]"))
+    SCATTERING_LENGTH_DENSITY = AfterValidator(partial(_check_dimension, "[area]"))
+    SOLID_ANGLE = AfterValidator(partial(_check_dimension, "[]"))
+    TEMPERATURE = AfterValidator(partial(_check_dimension, "[temperature]"))
+    TIME = AfterValidator(partial(_check_dimension, "[time]"))
+    TIME_OF_FLIGHT = AfterValidator(partial(_check_dimension, "[time]"))
+    UNITLESS = AfterValidator(partial(_check_dimension, "[]"))
+    VOLTAGE = AfterValidator(partial(_check_dimension, "[electric_potential]"))
+    VOLUME = AfterValidator(partial(_check_dimension, "[volume]"))
+    WAVELENGTH = AfterValidator(partial(_check_dimension, "[length]"))
+    WAVENUMBER = AfterValidator(partial(_check_dimension, "[]"))
 
 
 def QuantityType[T](is_type: type) -> Any:
@@ -41,7 +75,7 @@ class NXobject(BaseModel):
 
 
 __all__ = [
-    "NXUnit",
+    "Units",
     "QuantityType",
     "Field",
     "NXobject",
